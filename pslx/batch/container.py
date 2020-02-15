f98a64d20ed13c77f6d7134ed306a140bb71a77b
kfrancischen@gmail.com <kfrancischen@gmail.com>
2020-02-14 21:59:06 -0800

    add implementation of schedueld streaming container

--- pslx/batch/container.py
+++ pslx/batch/container.py
@@ -1,12 +1,14 @@
+from apscheduler.schedulers.background import BackgroundScheduler
 import os
 
 from pslx.core.container_base import ContainerBase
 from pslx.schema.enums_pb2 import DataModelType
 from pslx.tool.logging_tool import LoggingTool
 from pslx.util.proto_util import ProtoUtil
+from pslx.util.timezone_util import TimeZoneObj
 
 
-class BatchContainer(ContainerBase):
+class BasicBatchContainer(ContainerBase):
     DATA_MODEL = DataModelType.BATCH
 
     def __init__(self, container_name, tmp_file_folder='tmp/', ttl=-1):
@@ -17,3 +19,39 @@ class BatchContainer(ContainerBase):
             root_dir=os.getenv('DATA_ROOT_DIR', 'database/'),
             ttl=ttl
         )
+
+
+class ScheduledBatchContainer(BasicBatchContainer):
+    DATA_MODEL = DataModelType.BATCH
+
+    def __init__(self, container_name, tmp_file_folder='tmp/', ttl=-1):
+        super().__init__(container_name, tmp_file_folder=tmp_file_folder, ttl=ttl)
+        self._scheduler_spec = {
+            'day_of_week': None,
+            'hour': None,
+            'minute': None,
+            'second': None,
+        }
+
+    def set_schedule(self, day_of_week, hour, minute=None, second=None):
+        self._scheduler_spec = {
+            'day_of_week': day_of_week,
+            'hour': hour,
+            'minute': minute,
+            'second': second,
+        }
+        self._logger.write_log("Spec sets to " + str(self._scheduler_spec))
+        self.log_print("Spec sets to " + str(self._scheduler_spec))
+
+    def execute(self, is_backfill=False, num_process=1):
+        background_scheduler = BackgroundScheduler(timezone=TimeZoneObj.WESTERN_TIMEZONE)
+        background_scheduler.add_job(
+            super().execute,
+            'cron',
+            args=[is_backfill, num_process],
+            day_of_week=self._scheduler_spec['day_of_week'],
+            hour=self._scheduler_spec['hour'],
+            minute=self._scheduler_spec['minute'],
+            second=self._scheduler_spec['second']
+        )
+        background_scheduler.start()
