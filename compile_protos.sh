a85c942cc7b0ee681c3792c3dc3594af5ca7e146
kfrancischen@gmail.com <kfrancischen@gmail.com>
2020-03-03 23:21:34 -0800

    add rpc proto

--- compile_protos.sh
+++ compile_protos.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 for file in pslx/schema/*; do
-  if [[ $file == *.proto ]]
+  if [[ $file == *.proto && $file != pslx/schema/*rpc.proto ]]
   then
-    echo "Compiling: $file"
+    echo "Compiling regular proto: $file"
     protoc  --proto_path="pslx/schema" --python_out=pslx/schema/ "$file"
+  elif [[ $file == pslx/schema/*rpc.proto ]]
+  then
+    echo "Compiling rpc proto: $file"
+    python -m grpc_tools.protoc -Ipslx/schema/ --python_out=pslx/schema/ --grpc_python_out=pslx/schema/ "$file"
   fi
 
 done
 sed -i -e 's/import enums_pb2 as enums__pb2/import pslx.schema.enums_pb2 as enums__pb2/g' pslx/schema/snapshots_pb2.py
+sed -i -e 's/import enums_pb2 as enums__pb2/import pslx.schema.enums_pb2 as enums__pb2/g' pslx/schema/rpc_pb2.py
+sed -i -e 's/import rpc_pb2 as rpc__pb2/import pslx.schema.rpc_pb2 as rpc__pb2/g' pslx/schema/rpc_pb2_grpc.py
