0f0687113fa407906f749d9e4ddbfc7fa3ce9a48
kfrancischen@gmail.com <kfrancischen@gmail.com>
2020-03-11 09:23:47 -0700

    update

--- example/message_queue_example/consumer.py
+++ example/message_queue_example/consumer.py
@@ -4,6 +4,8 @@ from pslx.micro_service.message_queue.queue_base import QueueBase
 from pslx.micro_service.message_queue.generic_consumer import GenericConsumer
 from pslx.schema.enums_pb2 import Status
 from pslx.schema.rpc_pb2 import InstantMessagingRPCRequest
+from pslx.storage.partitioner_storage import DailyPartitionerStorage
+from pslx.util.file_util import FileUtil
 from pslx.util.timezone_util import TimezoneUtil
 
 
@@ -34,8 +36,18 @@ if __name__ == "__main__":
     consumer = GenericConsumer(
         connection_str='amqp://guest:guest@localhost:5672'
     )
+
+    partitioner_dir = FileUtil.join_paths_to_dir_with_mode(
+        root_dir='database/msg_queue/',
+        base_name='msg_queue_example',
+        ttl='1h'
+    )
+    storage = DailyPartitionerStorage()
+    storage.initialize_from_dir(dir_name=partitioner_dir)
+
     slack_queue = SlackQueue(
-        queue_name='slack_queue'
+        queue_name='slack_queue',
+        queue_storage=storage
     )
     consumer.bind_queue(exchange='slack_exchange', queue=slack_queue)
     consumer.start_consumer()
