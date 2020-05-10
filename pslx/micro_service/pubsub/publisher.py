b31481d2c081af913943bcfac60a4608e2521a0a
Francis Chen <kfrancischen@gmail.com>
2020-05-10 09:46:25 -0700

    minor update

--- pslx/micro_service/pubsub/publisher.py
+++ pslx/micro_service/pubsub/publisher.py
@@ -46,5 +46,7 @@ class Publisher(Base):
                 routing_key=self._topic_name,
                 body=base64.b64encode(message_str)
             )
+            self._logger.info("Succeeded in publishing the data to exchange " + self._exchange_name +
+                              " with topic name " + self._topic_name + '.')
         except Exception as err:
             self._logger.error("publish message failed with error " + str(err) + '.')
