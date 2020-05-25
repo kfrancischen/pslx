643d2b4c31785b254d9d47a292ae85c7b028b474
] <kfrancischen@gmail.com>
2020-05-25 18:30:22 +0000

    disable reloader

--- example/frontend_example/run_frontend.py
+++ example/frontend_example/run_frontend.py
@@ -5,4 +5,4 @@ PYTHONPATH=. python example/frontend_example/run_frontend.py
 from pslx.micro_service.frontend import pslx_frontend_ui_app
 
 if __name__ == "__main__":
-    pslx_frontend_ui_app.run(host='localhost', port=5001, debug=True)
+    pslx_frontend_ui_app.run(host='localhost', port=5001, debug=True, use_reloader=False)
